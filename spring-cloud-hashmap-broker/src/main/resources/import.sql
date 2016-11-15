c97bea1df3b74090003f4c1a4372a0ab91fc0070
aditya gupta <eddy8700@gmail.com>
2016-11-15 14:12:19 +0530

    Project Structure Change

--- spring-cloud-hashmap-broker/src/main/resources/import.sql
+++
@@ -1,2 +0,0 @@
-insert into services (id, name, description, bindable) values (LOCALTIME, LOCALTIME, 'hashmap-service broker', true);
-insert into plans (id, name, description, service_id) values (LOCALTIME, 'basic', 'Plan001', LOCALTIME);
